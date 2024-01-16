a1a54f5371d536492c50f36ffefe58f1dccb07f9
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2024-01-16 11:48:51 -0300

    .

---
+++ src/components/Menu.jsx
@@ -0,0 +1,11 @@
+function Menu() {
+    return (
+        <nav>
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">Equipe</a></li>
+          <li><a href="">Serviços</a></li>
+        </ul>
+      </nav>
+    )
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+    <h1>Olá next.js!</h1>
+
+      <main>
+        <h2>Seja bem-vindo ao next.js</h2>
+        <h3>Página inicial</h3>
+      </main>
+    </>
+  );
+}
